dc7f0836c3648099ceb000fc916b852919f8a948
fin <fin+github@xbhd.org>
2019-05-19 15:33:10 +0200

    add „Freundliche Grüße” to possible closings

--- fragdenstaat_at/settings/base.py
+++ fragdenstaat_at/settings/base.py
@@ -376,7 +376,7 @@ class FragDenStaatBase(German, Base):
             custom_replacements=[
                 rec(r'[Bb][Gg]-[Nn][Rr]\.?\s*\:?\s*([a-zA-Z0-9\s/]+)')
             ],
-            closings=[rec(r"([Mm]it )?(den )?(freundliche(n|m)?|vielen|besten)? ?Gr(ü|u)(ß|ss)(en?)?,?"), rec("Hochachtungsvoll,?"), rec(r'i\. ?A\.'), rec('[iI]m Auftrag')],
+            closings=[rec(r"([Mm]it )?(den )?([fF]reundliche(n|m)?|vielen|besten)? ?Gr(ü|u)(ß|ss)(en?)?,?"), rec("Hochachtungsvoll,?"), rec(r'i\. ?A\.'), rec('[iI]m Auftrag')],
             content_urls={
                 'terms': '/info/nutzungsbedingungen/',
                 'privacy': '/info/datenschutz/',

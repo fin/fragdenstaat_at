db3a248f5dd3d2fb58f4b771bd724a91065546c9
fin <fin+github@xbhd.org>
2021-12-14 17:49:40 +0100

    remove old/moved styles. fix homepage_how

--- fragdenstaat_at/frontend/javascript/misc/shuffle-items.ts
+++
@@ -1,6 +0,0 @@
-const containers = document.querySelectorAll('.shuffle-items')
-containers.forEach(container => {
-  for (let i = container.children.length; i >= 0; i--) {
-    container.appendChild(container.children[Math.random() * i | 0]);
-  }
-})

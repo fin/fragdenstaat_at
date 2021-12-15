3bd885e528aa064647ffa52e12a2cf4888e18820
fin <fin+github@xbhd.org>
2021-12-15 16:51:13 +0100

    remove matomo frontend

--- frontend/javascript/misc.ts
+++ frontend/javascript/misc.ts
@@ -1,6 +1,6 @@
 import "./misc/onion-darkmode"
 import "./misc/iframe-height"
-import "./misc/matomo"
+//import "./misc/matomo"
 import "./misc/reference-forms"
 import "./misc/secpol-violation"
 import "./misc/shuffle-items"

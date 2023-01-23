dcac2622bca5ae13865abb4e0e12c08192071e55
fin <fin+github@xbhd.org>
2023-01-23 02:43:22 +0100

    grab commits from fragdenstaat_de up to abe0781dc89b94f285814a7b9c71bf008171a156

---
+++ fragdenstaat_at/theme/views.py
@@ -0,0 +1,90 @@
+# from django.contrib import messages
+# from django.contrib.auth.decorators import login_required
+# from django.http import JsonResponse
+# from django.shortcuts import get_object_or_404, redirect, render
+# from django.views.decorators.http import require_POST
+
+from fcdocs_annotate.annotation.views import AnnotateDocumentView
+
+# from froide.account.models import UserPreference
+# from froide.foirequest.models import FoiMessage, FoiRequest
+# from froide.foirequest.views.request_actions import allow_write_foirequest
+# from froide.frontpage.models import FeaturedRequest
+# from froide.helper.cache import cache_anonymous_page
+# from froide.publicbody.models import PublicBody
+
+# from .forms import TippspielForm
+
+# from .glyphosat import get_glyphosat_document
+
+
+# @cache_anonymous_page(15 * 60)
+# def index(request):
+#     successful_foi_requests = FoiRequest.published.successful()[:6]
+#     featured = (
+#         FeaturedRequest.objects.all()
+#         .order_by("-timestamp")
+#         .select_related("request", "request__public_body")
+#     )
+#     return render(
+#         request,
+#         "index.html",
+#         {
+#             "featured": featured[:3],
+#             "successful_foi_requests": successful_foi_requests,
+#             "foicount": FoiRequest.objects.get_send_foi_requests().count(),
+#             "pbcount": PublicBody.objects.get_list().count(),
+#         },
+#     )
+
+
+# @require_POST
+# @allow_write_foirequest
+# def glyphosat_download(request, foirequest, message_id):
+#     message = get_object_or_404(FoiMessage, request=foirequest, pk=message_id)
+#     is_message = (
+#         message.request.campaign_id == 9
+#         and message.is_response
+#         and "Kennwort lautet:" in message.plaintext
+#     )
+#     if not is_message:
+#         messages.add_message(
+#             request, messages.ERROR, "Leider ist etwas schief gelaufen."
+#         )
+#         return redirect(foirequest)
+
+#     if not request.POST.get("confirm-notes") == "1":
+#         messages.add_message(
+#             request, messages.ERROR, "Sie müssen die Infos zur Kenntnis nehmen!"
+#         )
+#         return redirect(foirequest)
+
+#     result = get_glyphosat_document(message)
+#     if result:
+#         return redirect(result)
+
+#     messages.add_message(request, messages.ERROR, "Leider ist etwas schief gelaufen.")
+#     return redirect(foirequest)
+
+
+# @login_required
+# def meisterschaften_tippspiel(request):
+#     error = None
+#     if request.method == "POST":
+#         form = TippspielForm(request.POST)
+#         if form.is_valid():
+#             form.save(request.user)
+#         else:
+#             error = " ".join(" ".join(v) for k, v in form.errors.items())
+
+#     prefs = UserPreference.objects.get_preferences(
+#         request.user, key_prefix="fds_meisterschaften_2020_"
+#     )
+
+#     return JsonResponse({"user": prefs, "error": error})
+
+
+class FDSAnnotationView(AnnotateDocumentView):
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(portal__isnull=True)
